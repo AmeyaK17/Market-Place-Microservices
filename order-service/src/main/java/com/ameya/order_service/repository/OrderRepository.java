ead7765426a0c30273f11c646c5cbfd871073655
Ameya Kale <ameya.k.kale@gmail.com>
2024-06-27 18:48:00 -0400

    Added Order Service

---
+++ order-service/src/main/java/com/ameya/order_service/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.ameya.order_service.repository;
+
+import com.ameya.order_service.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
---
+++ order-service/src/main/java/com/ameya/order_service/service/OrderService.java
@@ -0,0 +1,42 @@
+package com.ameya.order_service.service;
+
+import com.ameya.order_service.dto.OrderLineItemsDto;
+import com.ameya.order_service.dto.OrderRequest;
+import com.ameya.order_service.model.Order;
+import com.ameya.order_service.model.OrderLineItems;
+import com.ameya.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItemsList);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+
+        return orderLineItems;
+    }
+}
